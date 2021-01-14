f01ca6435ec4856d33259623396e98d5b793a8ad
Gokul P B <gokulpbharathan89@gmail.com>
2021-01-14 13:00:04 +0530

    Add printing string

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import os
 import sys
+import xlsxwriter
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
 
 master_filename = sys.argv[1]
 
@@ -10,13 +15,20 @@ name_data = pd.read_excel(os.path.join(master_filename), engine='openpyxl', usec
 # print(name_data)
 
 present_names = pd.read_excel(os.path.join(present_filename), engine='openpyxl', usecols='A').values.tolist()
-# print(present_names)
 
 master = name_data
-# print(master)
+
 persons = present_names
-# print(persons)
+
+def listToString(s):  
+    str1 = ""  
+    for ele in s:  
+        str1 += ele     
+    return str1  
 
 for name in master:
     if name not in persons:
-        print(name)
+        strname = listToString(name)
+        print(f" {Back.BLACK + Fore.GREEN + strname}")
+
+
